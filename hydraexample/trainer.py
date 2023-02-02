4e32360a7eb293ee76e707b266a37fa7ce0813b6
Arun A. Kumar <arun.anthony.kumar@gmail.com>
2023-02-02 19:47:19 +0000

    Move files

--- hydraexample/trainer.py
+++ hydraexample/trainer.py
@@ -1,3 +1,6 @@
+from hydraexample.optimizer.optimizer import Optimizer
+from hydraexample.dataset.dataset import Dataset
+
 class Trainer:
     def __init__(self, optimizer: Optimizer, dataset: Dataset) -> None:
         self.optimizer = optimizer
