310b0fd36c055190eab6a3c513dce9ca7b559768
Arun Kumar <arun.anthony.kumar@gmail.com>
2023-02-03 03:35:56 +0000

    Add model

--- hydraexample/trainer.py
+++ hydraexample/trainer.py
@@ -1,7 +1,8 @@
 from hydraexample.optimizer.optimizer import Optimizer
 from hydraexample.dataset.dataset import Dataset
+from flax import linen as nn
 
 class Trainer:
-    def __init__(self, optimizer: Optimizer, dataset: Dataset) -> None:
+    def __init__(self, optimizer: Optimizer, dataset: Dataset, model: nn.Module) -> None:
         self.optimizer = optimizer
         self.dataset = dataset
